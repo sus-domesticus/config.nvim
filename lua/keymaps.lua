3285bf9ce79190d06d0a48cbb014246f2f9f50bf
sus-domesticus <susdomesticus@tutamail.com>
2024-03-23 21:59:10 +0200

    feat: add shorcut for discard register

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,5 @@
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set({ "n", "v" }, "-", '"_')
 
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
