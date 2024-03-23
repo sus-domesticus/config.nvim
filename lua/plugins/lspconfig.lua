b08351383a2ec09d79292fe804ae20c4c2fb831b
sus-domesticus <susdomesticus@tutamail.com>
2024-03-23 21:40:20 +0200

    fix: use default opts for neodev

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -6,12 +6,7 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 			{ "j-hui/fidget.nvim", opts = {} },
-			{
-				"folke/neodev.nvim",
-				opts = {
-					library = { plugins = { "telescope.nvim", "nvim-dap", "nvim-dap-ui" }, types = true },
-				},
-			},
+			{ "folke/neodev.nvim", opts = {} },
 		},
 		config = function()
 			vim.api.nvim_create_autocmd("LspAttach", {
