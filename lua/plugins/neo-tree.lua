c1f48ea05ad534efefd89f9cfd519da24d5a9572
sus-domesticus <susdomesticus@tutamail.com>
2024-03-24 10:52:39 +0200

    feat: neo-tree netrw hijack

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,13 @@ return {
 		"MunifTanjim/nui.nvim",
 		"3rd/image.nvim",
 	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				hijack_netrw_behavior = "open_current",
+			},
+		})
+	end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
