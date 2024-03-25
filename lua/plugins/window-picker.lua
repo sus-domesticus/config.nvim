e0b42bed156c820fe9aeff318dae4df017d2da60
sus-domesticus <susdomesticus@tutamail.com>
2024-03-25 20:02:58 +0200

    plugin: add window-picker

---
+++ lua/plugins/window-picker.lua
@@ -0,0 +1,11 @@
+return {
+	"s1n7ax/nvim-window-picker",
+	name = "window-picker",
+	event = "VeryLazy",
+	version = "2.*",
+	config = function()
+		require("window-picker").setup()
+	end,
+}
+
+-- vim: ts=2 sts=2 sw=2 et
