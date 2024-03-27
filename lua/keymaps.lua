605a14a057a1c3f241f1dc632be493121090e8e7
sus-domesticus <susdomesticus@tutamail.com>
2024-03-27 22:35:18 +0200

    feat: add resize keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -18,10 +18,15 @@ vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagn
 
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
-vim.keymap.set("t", "<c-h>", "<cmd>wincmd h <cr>")
-vim.keymap.set("t", "<c-j>", "<cmd>wincmd j <cr>")
-vim.keymap.set("t", "<c-k>", "<cmd>wincmd k <cr>")
-vim.keymap.set("t", "<c-l>", "<cmd>wincmd l <cr>")
+vim.keymap.set("t", "<c-h>", "<cmd>wincmd h<cr>")
+vim.keymap.set("t", "<c-j>", "<cmd>wincmd j<cr>")
+vim.keymap.set("t", "<c-k>", "<cmd>wincmd k<cr>")
+vim.keymap.set("t", "<c-l>", "<cmd>wincmd l<cr>")
+
+vim.keymap.set({ "n", "t" }, "<C-Up>", "<cmd>resize -2<cr>", { desc = "Resize -2" })
+vim.keymap.set({ "n", "t" }, "<C-Down>", "<cmd>resize +2<cr>", { desc = "Resize +2" })
+vim.keymap.set({ "n", "t" }, "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Vertical resize -2" })
+vim.keymap.set({ "n", "t" }, "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Vertical resize +2" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
