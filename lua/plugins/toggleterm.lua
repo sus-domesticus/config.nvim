a177ae4ea86b62b29e43580393ff8a978af86acd
sus-domesticus <susdomesticus@tutamail.com>
2024-03-27 21:09:28 +0200

    feat(keymaps): add for toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,17 @@
 return {
 	"akinsho/toggleterm.nvim",
 	version = "*",
-	config = true,
+	config = function()
+		require("toggleterm").setup()
+
+		vim.keymap.set("n", "<leader>tt", function()
+			if vim.v.count == 0 then
+				vim.cmd("ToggleTerm")
+				return
+			end
+			vim.cmd('exe v:count . "ToggleTerm"')
+		end, { desc = "[T]oggle [t]erminal" })
+	end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
