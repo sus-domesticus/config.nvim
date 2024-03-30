e99b0a1743613e3436fc8beb69d4438bdc204b3f
sus-domesticus <susdomesticus@tutamail.com>
2024-03-30 12:46:59 +0200

    fix dap-ui stuff

--- lua/plugins/nvim-dap-ui.lua
+++ lua/plugins/nvim-dap-ui.lua
@@ -6,52 +6,47 @@ return {
 	},
 	opts = {},
 	config = function(opts)
-		require("dapui").setup(opts)
+		vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "", linehl = "", numhl = "" })
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("dap-ui-lsp-attach", { clear = true }),
 			callback = function(event)
+				local success, dapSetup =
+					pcall(require, "plugins.helpers.nvim-dap-ui.dap-" .. vim.bo.filetype .. "-setup")
+				if not success then
+					return
+				end
+				local dap = require("dap")
+				dapSetup.setup(dap)
+
+				local dapui = require("dapui")
+				dapui.setup(opts)
+
 				local map = function(keys, func, desc)
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "DAP: " .. desc })
 				end
 
 				map("<leader>B", function()
-					require("dapui").toggle()
+					dapui.toggle()
 				end, "toggle UI")
 
 				map("<F10>", function()
-					require("dap").step_over()
+					dap.step_over()
 				end, "step over")
 
 				map("<F11>", function()
-					require("dap").step_into()
+					dap.step_into()
 				end, "step into")
 
 				map("<F12>", function()
-					require("dap").step_out()
+					dap.step_out()
 				end, "step out")
 
 				map("<leader>b", function()
-					require("dap").toggle_breakpoint()
+					dap.toggle_breakpoint()
 				end, "toggle breakpoint")
 
 				map("<F5>", function()
-					if vim.bo.filetype == "" then
-						print("No dap for empty filetype.")
-						return
-					end
-
-					local dap = require("dap")
-
-					local success, dapSetup =
-						pcall(require, "plugins.helpers.nvim-dap-ui.dap-" .. vim.bo.filetype .. "-setup")
-					if success and dapSetup ~= nil and dapSetup.setup ~= nil then
-						dapSetup.setup(dap)
-					else
-						print("You need a dap-" .. vim.bo.filetype .. "-setup.")
-						return
-					end
-
 					dap.continue()
 				end, "continue")
 			end,
