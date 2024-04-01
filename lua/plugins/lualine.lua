a115623e01d6091ef0383ea7138c910cb2ac77c7
MGreek <marcgrec2003@gmail.com>
2024-04-02 01:34:23 +0300

    feat: add session-path to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,29 @@
+local function getCurrentSession()
+	if vim.g.persisted_loaded_session == nil then
+		return ""
+	end
+	local path = vim.fs.basename(vim.g.persisted_loaded_session)
+	local delimiter = vim.fn.has("win32") == 1 and "\\" or "/"
+	local result = ""
+	for c in path:gmatch(".") do
+		if c == "%" then
+			result = result .. delimiter
+		else
+			result = result .. c
+		end
+	end
+	return result
+end
+
 return {
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		options = { theme = "auto" },
-		winbar = { lualine_a = { "buffers" } },
-	},
+	config = function()
+		require("lualine").setup({
+			options = { theme = "auto" },
+			winbar = { lualine_a = { "buffers" }, lualine_z = { getCurrentSession } },
+		})
+	end,
 }
 
 -- vim: ts=2 sts=2 sw=2 et
