3d45cade0c608475f7ed5012b2fcd618f8ad1914
sus-domesticus <susdomesticus@tutamail.com>
2024-04-01 14:34:52 +0300

    fix: use powershell workaround
    see :h shell-powershell

--- lua/options.lua
+++ lua/options.lua
@@ -41,4 +41,19 @@ vim.opt.foldenable = true
 
 vim.opt.hidden = true
 
+-- windows only
+if vim.fn.has("win32") then
+	-- user powershell
+	vim.api.nvim_exec2(
+		[[
+      let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+      let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+      let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+      let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+      set shellquote= shellxquote=
+    ]],
+		{}
+	)
+end
+
 -- vim: ts=2 sts=2 sw=2 et
