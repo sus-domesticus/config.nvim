29cb7f6300efecddf8b42b01c4115bd0cf9cbb1f
MGreek <marcgrec2003@gmail.com>
2024-04-02 13:01:45 +0300

    feat: add bufresize support for undotree

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,11 +1,38 @@
+local function find_open()
+	local wins = {}
+	for _, win in pairs(vim.api.nvim_list_wins()) do
+		local buf = vim.api.nvim_win_get_buf(win)
+		if vim.bo[buf].filetype == "undotree" then
+			table.insert(wins, win)
+		end
+	end
+	return wins
+end
+
 return {
 	{
 		"jiaoshijie/undotree",
 		dependencies = "nvim-lua/plenary.nvim",
-		config = true,
-		keys = {
-			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "[U]ndotree" },
-		},
+		config = function()
+			local undotree = require("undotree")
+			undotree.setup()
+
+			vim.keymap.set("n", "<leader>u", function()
+				local bufresize = require("bufresize")
+				bufresize.register()
+				bufresize.block_register()
+
+				local open = find_open()
+
+				if #open == 0 then
+					undotree.open()
+					bufresize.resize_open()
+				else
+					undotree.close()
+					bufresize.resize_close()
+				end
+			end, { desc = "[U]ndotree" })
+		end,
 	},
 }
 
