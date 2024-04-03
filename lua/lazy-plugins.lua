288dd9b6333949e5fa8c161290aea24a59cf2af4
sus-domesticus <susdomesticus@tutamail.com>
2024-04-03 23:51:44 +0300

    plugin: add ts-autotag

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -4,6 +4,7 @@ require("lazy").setup({
 	{ "numToStr/Comment.nvim", opts = {} },
 	"famiu/bufdelete.nvim",
 	{ "jokajak/keyseer.nvim", version = "*", opts = {} },
+  require("plugins.ts-autotag"),
 	require("plugins.satellite"),
 	require("plugins.persisted"),
 	require("plugins.zen-mode"),
---
+++ lua/plugins/ts-autotag.lua
@@ -0,0 +1,5 @@
+return {
+	"windwp/nvim-ts-autotag",
+	opts = {},
+}
+-- vim: ts=2 sts=2 sw=2 et
