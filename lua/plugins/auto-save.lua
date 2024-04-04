95f3cd314106fbe8eb133e47c994569d2134ce7c
MGreek <marcgrec2003@gmail.com>
2024-04-04 22:03:32 +0300

    feat: auto-save less frequent

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,7 +2,9 @@ return {
 	"okuuva/auto-save.nvim",
 	cmd = "ASToggle",
 	event = { "InsertLeave", "TextChanged" },
-	opts = {},
+	opts = {
+		debounce_delay = 120 * 1000, -- 120 seconds
+	},
 }
 
 -- vim: ts=2 sts=2 sw=2 et
