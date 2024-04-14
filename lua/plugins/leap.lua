06a99cdaa269b6f4b8a45e811a6f9513bfb8b848
MGreek <marcgrec2003@gmail.com>
2024-04-14 21:53:41 +0300

    feat: add equiv classes for leap.nvim

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -2,6 +2,9 @@ return {
   "ggandor/leap.nvim",
   dependencies = { "tpope/vim-repeat" },
   config = function()
+    local leap = require("leap")
+    leap.opts.equivalence_classes =
+      { "\t\r\n", "aäăâ", "AÄĂÂ", "iî", "IÎ", "oö", "OÖ", "uü", "UÜ", "sșß", "SȘ", "tț", "TȚ" }
     vim.keymap.set({ "n", "v", "o" }, "<leader><leader>", "<Plug>(leap)", { desc = "Leap" })
     vim.keymap.set("n", "<C-w><leader>", "<Plug>(leap-from-window)", { desc = "Leap from window" })
   end,
