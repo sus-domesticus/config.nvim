2652de25dac353ee022758036a3bd7348a351a72
MGreek <marcgrec2003@gmail.com>
2024-05-04 06:21:01 +0300

    feat: equiv classes for leap plug

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local leap = require("leap")
     leap.opts.equivalence_classes =
-      { "\t\r\n", "aäăâ", "AÄĂÂ", "iî", "IÎ", "oö", "OÖ", "uü", "UÜ", "sșß", "SȘ", "tț", "TȚ" }
+      { " \t\r\n", "aäăâ", "AÄĂÂ", "iî", "IÎ", "oö", "OÖ", "uü", "UÜ", "sșß", "SȘ", "tț", "TȚ" }
     vim.keymap.set({ "n", "v", "o" }, "<leader><leader>", "<Plug>(leap)", { desc = "Leap" })
     vim.keymap.set("n", "<C-w><leader>", "<Plug>(leap-from-window)", { desc = "Leap from window" })
   end,
