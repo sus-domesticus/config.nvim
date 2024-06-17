50e2babe96640b4314ddd97722b4d26aa358031e
MGreek <marcgrec2003@gmail.com>
2024-06-17 15:51:41 +0300

    fix powershell hack

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ vim.opt.hlsearch = true
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- HACK: powershell hack for windows
-if vim.uv.os_uname().sysname == "Windows" then
+if vim.uv.os_uname().sysname == "Windows_NT" then
   vim.cmd([[let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
     let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
     let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
