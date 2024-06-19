dec2044ca4131ec81c9fb93ec2f202c1956cc096
MGreek <marcgrec2003@gmail.com>
2024-06-19 22:01:04 +0300

    add eslint_d linter

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -4,6 +4,7 @@ return {
     config = function()
       require("lint").linters_by_ft = {
         lua = { "luacheck" },
+        typescript = { "eslint_d" },
       }
       vim.api.nvim_create_autocmd({ "BufWritePost" }, {
         callback = function()
