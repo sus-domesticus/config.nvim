1a388e795ce8ff87fa8651db8da1037ba8800832
susd <susdomesticus@tutamail.com>
2024-07-05 23:03:10 +0300

    add luasnip

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tex" },
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,6 +4,21 @@ return {
     "https://codeberg.org/FelipeLema/cmp-async-path",
   },
   opts = function(_, opts)
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    opts.mapping = cmp.mapping.preset.insert({
+      ["<C-l>"] = cmp.mapping(function()
+        if luasnip.expand_or_locally_jumpable() then
+          luasnip.expand_or_jump()
+        end
+      end, { "i", "s" }),
+      ["<C-h>"] = cmp.mapping(function()
+        if luasnip.locally_jumpable(-1) then
+          luasnip.jump(-1)
+        end
+      end, { "i", "s" }),
+    })
+
     for index, entry in ipairs(opts.sources) do
       if entry.name == "path" then
         table.remove(opts.sources, index)
