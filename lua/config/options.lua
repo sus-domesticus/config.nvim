cb00f910ff552f262c8321b134b9695160ff7eb4
susd <susdomesticus@tutamail.com>
2024-07-08 20:21:32 +0300

    add number and relativenumber options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.o.number = true
+vim.o.relativenumber = true
+
 -- HACK: powershell hack for windows; see `:h shell-powershell`
 if vim.uv.os_uname().sysname == "Windows_NT" then
   vim.cmd([[let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
