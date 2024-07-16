331c0f8237bad0fe18d03ff2e9530d0bed6ba21a
susd <susdomesticus@tutamail.com>
2024-07-16 20:34:27 +0300

    remove unused plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -49,6 +49,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "plugins" },
+    { "folke/persistence.nvim", enabled = false },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
