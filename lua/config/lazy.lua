8562d8b45fc6c1a5ed8e71b627bd4fe7c1ea7bec
MGreek <marcgrec2003@gmail.com>
2024-08-02 13:26:23 +0300

    add omnisharp support

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -45,6 +45,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
+    { import = "lazyvim.plugins.extras.lang.omnisharp" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.vscode" },
