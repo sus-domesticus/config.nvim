b816fb06a1aa154dc6aa9354dbc31222b9391a80
susd <susdomesticus@tutamail.com>
2024-08-23 22:02:59 +0300

    add support for nix

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,6 +42,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.kotlin" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.nix" },
     { import = "lazyvim.plugins.extras.lang.cmake" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
